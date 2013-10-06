bcfd57c7310374d7fc13db422c5047a8575044af
Blue Carpet <andrey.vashkevich@gmail.com>
2013-10-06 17:53:42 +0300

    Added new branch C++; switched to C++; implemented camera frame capturing.

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include "opencv2/core/core.hpp"
+#include "opencv2/flann/miniflann.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/photo/photo.hpp"
+#include "opencv2/video/video.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/ml/ml.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/contrib/contrib.hpp"
+#include "opencv2/core/core_c.h"
+#include "opencv2/highgui/highgui_c.h"
+#include "opencv2/imgproc/imgproc_c.h"
+
+using namespace std;
+using namespace cv; 
+
+int main()
+{
+	CvCapture* capture = cvCreateCameraCapture(CV_CAP_ANY);
+	IplImage* frame = 0;
+	
+	cvNamedWindow("Camera", CV_WINDOW_AUTOSIZE);
+	while (true)
+	{
+		frame = cvQueryFrame(capture);
+		cvShowImage("Camera", frame);
+		if ((cvWaitKey(10) & 255) == 27)
+		{
+			break;
+		}
+	}
+	return 0;
+}
