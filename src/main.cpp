f8807c8d5729ebaf56e5f508f130a878f18b2306
Blue Carpet <andrey.vashkevich@gmail.com>
2013-10-13 18:20:43 +0300

    Implementing command-line arguments support.

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,32 @@
 
 #include "recognizer.h"
 
-int main()
-{
+int main(int argc, char* argv[]) {
+
     std::string answer;
     anpr::Recognizer r;
     CvCapture* capture = cvCreateCameraCapture(CV_CAP_ANY);
 
-    cvNamedWindow("Camera", 0);
-    cvResizeWindow("Camera", 640, 350);
-    while (true)
-    {
+	bool showGui = (argc > 1) && (!strcmp(argv[1], "gui"));
+
+	if (showGui) {
+		cvNamedWindow("Camera", 0);
+		cvResizeWindow("Camera", 640, 350);
+	}
+    
+	while (true) {
         IplImage* frame = cvQueryFrame(capture);
         cv::Mat frameMat = frame;
         if (r.RecognizePlateNumber(frameMat, answer)) {
             std::cout << answer << std::endl;
         }
-        cv::imshow("Camera", frameMat);
-        if ((cvWaitKey(10) & 255) == 27)
-        {
-            break;
-        }
+		if (showGui) {
+			cv::imshow("Camera", frameMat);
+			if ((cvWaitKey(10) & 255) == 27) {
+				break;
+			}
+		}
     }
 
     return 0;
 }
-
