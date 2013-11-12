8e04778f5edeffc2af721e6369326d8ed8911e73
kolesov93 <kolesov93@gmail.com>
2013-11-12 16:55:42 +0300

    Added comments for recognizer.h

--- src/recognizer.h
+++ src/recognizer.h
@@ -3,6 +3,10 @@
 #include <opencv2/opencv.hpp>
 #include <string>
 
+/*
+Using pointer to implementation idiom. It allows fast rebuilding of project and easy testing
+*/
+
 namespace anpr {
 
 class Recognizer {
@@ -13,8 +17,13 @@ private:
     Impl* impl_;
 
 public:
+    // learn_path - path to directory with test sample
     Recognizer(const std::string& learn_path);
     ~Recognizer();
+    // Method to recognize plate number.
+    // Returns if number was recognized
+    // image - object, that incaplulates image
+    // value - object, where result will be stored
     bool RecognizePlateNumber(cv::Mat image, std::string& value);
 };
 
